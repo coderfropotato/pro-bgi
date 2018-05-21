7edfa8032ea5cef70e1b210138c3572cbb33cb4b
xiaofang <1692977129@qq.com>
2018-05-21 19:56:30 +0800

    done

--- webroot/res/scripts/super/directive/littleTableDire.js
+++ webroot/res/scripts/super/directive/littleTableDire.js
@@ -1,15 +1,16 @@
 /**
  * 小表
+ * <div class="little-table" page-entity="pageEntity" url="url" table-download-name="tableDownloadName" panel-id="panelId" table-panel-id="tablePanelId"></div>
  * 下载  刷新
  * 2018年5月21日16:46:17
  */
 
-define("superApp.littleTableDire",
-    ["angular", "super.superMessage", "select2"],
-    function (angular, SUPER_CONSOLE_MESSAGE) {
+define("superApp.littleTableDire", ["angular", "super.superMessage", "select2"],
+    function(angular, SUPER_CONSOLE_MESSAGE) {
         var superApp = angular.module("superApp.littleTableDire", []);
         superApp.directive('littleTable', littleTableDirective);
         littleTableDirective.$inject = ["$log"];
+
         function littleTableDirective($log) {
             return {
                 restrict: "ACE",
@@ -20,6 +21,13 @@ define("superApp.littleTableDire",
                     tableDownloadName: "=",
                     panelId: "=",
                     tablePanelId: "=",
+
+                    isHasSelectOption: "=",
+                    selectOptionList: "=",
+                    selectModel: "=",
+
+                    isHasAccuracy: "=",
+                    accuracy: "="
                 },
                 replace: false,
                 transclude: true,
@@ -29,12 +37,17 @@ define("superApp.littleTableDire",
 
         superApp.controller("littleTableCtr", littleTableCtr);
         littleTableCtr.$inject = ["$rootScope", "$scope", "$log", "$state", "$window", "ajaxService", "toolService", "reportService"];
+
         function littleTableCtr($rootScope, $scope, $log, $state, $window, ajaxService, toolService, reportService) {
-            $scope.InitPage = function () {
+            $scope.InitPage = function() {
+                if ($scope.isHasSelectOption) {
+                    $scope.selectModel = $scope.selectOptionList[0];
+                }
+                $scope.accuracy = -1;
                 $scope.GetTableData();
             }
 
-            $scope.GetTableData = function () {
+            $scope.GetTableData = function() {
                 toolService.gridFilterLoading.open($scope.tablePanelId);
                 var ajaxConfig = {
                     data: $scope.pageEntity,
@@ -42,7 +55,7 @@ define("superApp.littleTableDire",
                 };
 
                 var promise = ajaxService.GetDeferData(ajaxConfig);
-                promise.then(function (responseData) {
+                promise.then(function(responseData) {
                     if (responseData.Error) {
                         $scope.error = "syserror";
                     } else if (responseData.length == 0) {
@@ -52,11 +65,10 @@ define("superApp.littleTableDire",
                         $scope.tableData = responseData;
                     }
                     toolService.gridFilterLoading.close($scope.tablePanelId);
-                }, function (errorMesg) {
+                }, function(errorMesg) {
                     toolService.gridFilterLoading.close($scope.tablePanelId);
                     $scope.error = "syserror";
                 });
             }
         }
-    });
-
+    })

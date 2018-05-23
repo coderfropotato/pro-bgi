6f00ed29e38ebdc078575643b3785b9b2451ac93
xiaofang <1692977129@qq.com>
2018-05-23 19:00:08 +0800

    done

--- webroot/res/scripts/super/directive/staticImgExportDire.js
+++ webroot/res/scripts/super/directive/staticImgExportDire.js
@@ -1,34 +1,37 @@
-
-define("superApp.staticImgExportDire",
-    ["angular", "super.superMessage", "select2"],
-    function (angular, SUPER_CONSOLE_MESSAGE) {
+define("superApp.staticImgExportDire", ["angular", "super.superMessage", "select2"],
+    function(angular, SUPER_CONSOLE_MESSAGE) {
         var superApp = angular.module("superApp.staticImgExportDire", []);
         /*
-        ** 创建日期：2017-06-19
-        ** 功能简介：D3导出单张图片
-        ** 形如：<div class="svg-new-export" chartid="div_ReadsQuYuFenBu_CharPanel" saveimgname="基因组区域比对分布统计" ></div>
-        ** class="svg-new-export"：指令，必需
-        ** chartid="div_ReadsQuYuFenBu_CharPanel"： chartid
-        ** saveimgname="基因组区域比对分布统计"： 要保存的文件名
-        */
+         ** 创建日期：2017-06-19
+         ** 功能简介：D3导出单张图片
+         ** 形如：<div class="svg-new-export" chartid="div_ReadsQuYuFenBu_CharPanel" saveimgname="基因组区域比对分布统计" ></div>
+         ** class="svg-new-export"：指令，必需
+         ** chartid="div_ReadsQuYuFenBu_CharPanel"： chartid
+         ** saveimgname="基因组区域比对分布统计"： 要保存的文件名
+         */
         superApp.directive("staticImgExport", staticImgExport);
         staticImgExport.$inject = ["$log", "$compile"];
+
         function staticImgExport($log, $compile) {
             return {
                 restrict: "ACE",
-                template: "<div class='dropdown drop-menu'>"
-                    + "<button class='btn btn-default btn-sm btn-silver tool-tip' title='导出'>"
-                    + "<span class='glyphicon glyphicon-picture'></span> "
+                template: "<div class='dropdown drop-menu'>" +
+                    "<button class='btn btn-default btn-sm btn-silver tool-tip' title='导出'>" +
+                    "<span class='glyphicon glyphicon-picture'></span> "
                     //+ "<span class='icon-caret-down'></span>"
-                    + "</button>"
-                    + "<ul class='dropdown-menu'>"
-                    + "</ul>"
-                    + "</div>",
+                    +
+                    "</button>" +
+                    "<ul class='dropdown-menu'>" +
+                    "</ul>" +
+                    "</div>",
                 scope: {
-                    pdfExportUrl: "="
+                    // pdfExportUrl: "=",
+                    isExportPdf: "=",
+                    findEntity: "=",
+                    exportLocation: "="
                 },
                 controller: "staticImgExportController",
-                link: function (scope, element, attrs) {
+                link: function(scope, element, attrs) {
                     var $element = $(element);
                     var $dropdownMenu = $element.find(".dropdown-menu:eq(0)");
 
@@ -36,10 +39,9 @@ define("superApp.staticImgExportDire",
                     var LI_2 = $compile("<li><a ng-click=\"export('" + attrs.chartid + "', '" + attrs.saveimgname + "', 'jpg')\" href=\"javascript:;\">导出 JPG 格式图片</a></li>")(scope);
                     var LI_3 = $compile("<li><a ng-click=\"export('" + attrs.chartid + "', '" + attrs.saveimgname + "', 'pdf')\" href=\"javascript:;\">导出 PDF 格式文件</a></li>")(scope);
 
-
                     $dropdownMenu.append(LI_1);
                     $dropdownMenu.append(LI_2);
-                    if(scope.pdfExportUrl) $dropdownMenu.append(LI_3);
+                    if (scope.isExportPdf) $dropdownMenu.append(LI_3);
                 }
             };
         };
@@ -47,8 +49,9 @@ define("superApp.staticImgExportDire",
 
         superApp.controller("staticImgExportController", staticImgExportController);
         staticImgExportController.$inject = ["$scope", "$log", "$state", "$window", "$compile", "ajaxService", "toolService"];
+
         function staticImgExportController($scope, $log, $state, $window, $compile, ajaxService, toolService) {
-            $scope.export = function (chartid, saveImgName, type) {
+            $scope.export = function(chartid, saveImgName, type) {
                 if (!saveImgName) {
                     saveImgName = "图表";
                 }
@@ -94,45 +97,68 @@ define("superApp.staticImgExportDire",
                 } else {
                     var image = new Image();
                     image.src = img_src;
-                    image.onload = function () {
+                    image.onload = function() {
                         base64 = getBase64Image(image);
                         download();
                     }
                 }
 
-                function download(){
+                function download() {
                     if (type != "pdf") {
                         var a = document.createElement('a');
                         a.href = base64;
                         a.download = type == "image/png" ? saveImgName + ".png" : saveImgName + ".jpg";
                         a.click();
                     } else {
-                        // pdf
-                        var oReq = new XMLHttpRequest();
-                        var URLToPDF = $scope.pdfExportUrl;
-                        oReq.open("POST", URLToPDF, true);
-                        oReq.responseType = "blob";
-    
-                        oReq.onreadystatechange = function () {
-                            if (oReq.readyState === 4) {
-                                if (oReq.status >= 200 && oReq.status < 300 || oReq.status == 304) {
-                                    var file = new Blob([oReq.response], {
-                                        type: 'application/pdf'
-                                    });
-                                    saveAs(file, saveImgName + ".pdf");
+                        // // pdf
+                        // var oReq = new XMLHttpRequest();
+                        // var URLToPDF = $scope.pdfExportUrl;
+                        // oReq.open("POST", URLToPDF, true);
+                        // oReq.responseType = "blob";
+
+                        // oReq.onreadystatechange = function() {
+                        //     if (oReq.readyState === 4) {
+                        //         if (oReq.status >= 200 && oReq.status < 300 || oReq.status == 304) {
+                        //             var file = new Blob([oReq.response], {
+                        //                 type: 'application/pdf'
+                        //             });
+                        //             saveAs(file, saveImgName + ".pdf");
+                        //         }
+                        //     }
+                        // }
+
+                        // oReq.send();
+
+                        $scope.findEntity.IsExportReport = true;
+                        toolService.pageLoading.open("正在为您导出pdf，请稍候...", 200);
+                        var ajaxConfig = {
+                            data: $scope.findEntity,
+                            url: $scope.exportLocation
+                        };
+                        var promise = ajaxService.GetDeferData(ajaxConfig);
+                        promise.then(function(responseData) {
+                                if (responseData.Error) {
+                                    //系统异常
+                                    toolService.popMesgWindow("数据导出异常，请及时联系系统管理员！");
+                                } else {
+                                    //正常
+                                    var fileInfo = responseData;
+                                    console.log(fileInfo);
                                 }
-                            }
-                        }
-    
-                        oReq.send();
+                                toolService.pageLoading.close();
+                            },
+                            function(errorMesg) {
+                                toolService.pageLoading.close();
+                                toolService.popMesgWindow("数据导出异常，请及时联系系统管理员！");
+                            });
+
                     }
                 }
 
-                
+
             };
 
         }
 
 
     });
-

9347c3271ce59f93cc52941724a7b6340a07560b
yangwd <yangwd@gooalgene.com>
2018-06-12 19:48:23 +0800

    Add : line tool

--- webroot/ps/tools/pages/lineController.js
+++ webroot/ps/tools/pages/lineController.js
@@ -1,15 +1,203 @@
-define(['toolsApp'], function(toolsApp) {
+define(['toolsApp'], function (toolsApp) {
     toolsApp.controller('lineController', lineController);
     lineController.$inject = ["$rootScope", "$scope", "$log", "$state", "$timeout", "$window", "$compile", "ajaxService", "toolService", "svgService", "reportService"];
 
     function lineController($rootScope, $scope, $log, $state, $timeout, $window, $compile, ajaxService, toolService, svgService, reportService) {
-
-        $scope.InitPage = function() {
-            $timeout(function() {
-                console.log(1);
+        toolService.pageLoading.open();
+        $scope.InitPage = function () {
+            $timeout(function () {
+                toolService.pageLoading.close();
             }, 300)
 
-            $scope.test = 'line';
+            $scope.id = $state.params.id;
+            $scope.title = '折线图 ( ID：' + $scope.id + " ) ";
+            $scope.lineError = false;
+            $scope.curType = '';
+            var g = JSON.parse(toolService.sessionStorage.get('CompareGroupList'));
+            var sc = JSON.parse(toolService.sessionStorage.get('SampleDiffList'));
+            var s = toolService.sessionStorage.get('SampleList');
+            s = s.split(',');
+            var sl = [];
+            s.forEach(function (val, index) {
+                sl.push({
+                    name: val,
+                    type: 'sample'
+                })
+            })
+            var o = g.concat(sc);
+            var gl = [];
+            o.forEach(function (val, index) {
+                gl.push({
+                    name: val.name,
+                    type: 'compareGroup'
+                });
+            })
+            $scope.list = gl.concat(sl);
+
+            $scope.GetLineChartData();
+            $scope.GetLinks();
+        }
+
+        $scope.GetLineChartData = function (flag) {
+            toolService.gridFilterLoading.open("analysis-linePanel");
+            var ajaxConfig = {
+                data: {
+                    "LCID": toolService.sessionStorage.get('LCID'),
+                    "id": $scope.id
+                },
+                url: options.api.mrnaseq_url + "/FoldLine/RE"
+            }
+            var promise = ajaxService.GetDeferData(ajaxConfig);
+            promise.then(function (res) {
+                if (res.Error) {
+                    $scope.lineError = "syserror";
+                } else if (res.length == 0) {
+                    $scope.lineError = "nodata";
+                } else {
+                    $scope.lineError = false;
+                    var x = res.baseThead[0].x;
+                    $scope.chartData = [];
+                    // $scope.chartData.length = res.baseThead.length - 1;
+                    $scope.mapTheadJson = {};
+                    res.baseThead.forEach(function (val, index) {
+                        if (index != 0) {
+                            for (var key in val) {
+                                $scope.mapTheadJson[val[key]] = key;
+                            }
+                        }
+                    })
+                    res.rows.forEach(function (val, index) {
+                        // $scope.chartData[index] = [];
+                        for (var name in val) {
+                            if (name !== x) {
+                                $scope.chartData.push({
+                                    category: val[x],
+                                    key: $scope.mapTheadJson[name],
+                                    value: val[name]
+                                });
+
+                            }
+                        }
+                    })
+
+                    var flag = '';
+                    for (var key in res.baseThead[1]) {
+                        flag = key;
+                    }
+                    $scope.list.forEach(function (val, index) {
+                        if (val.name === flag) {
+                            $scope.curType = val.type;
+                        }
+                    })
+
+                    $scope.drawLineChart($scope.chartData);
+                }
+                toolService.gridFilterLoading.close("analysis-linePanel");
+
+            }, function (errMsg) {
+                $scope.lineError = "syserror";
+                toolService.gridFilterLoading.close("analysis-linePanel");
+            })
+        }
+
+
+        //画折线图
+        $scope.lineOptions = {
+            "id": "",
+            "type": "line",
+            "data": [],
+            "width": 1000,
+            "titleBox": {
+                "show": false,
+                "position": "bottom"
+            },
+            "legendBox": {
+                "show": true,
+                "position": "right"
+            },
+            "axisBox": {
+                "xAxis": {
+                    "type": "discrete",
+                    "fontRotate": "auto",
+                    "title": $scope.curType
+                },
+                "yAxis": {
+                    "title": "log10(FPKM+1)"
+                }
+            },
+            "dataBox": {
+                "normalColor": angular.copy($rootScope.colorArr)
+            }
+        }
+        $scope.drawLineChart = function (data) {
+            $("#lineChart_panel").html("");
+
+            var width = $("#analysis-linePanel .graph_header").eq(0).width();
+
+            $scope.lineOptions.id = "lineChart_panel";
+            $scope.lineOptions.data = data;
+            $scope.lineOptions.width = width * 0.8;
+
+
+            $scope.linechart = new gooal.lineInit("#lineChart_panel", $scope.lineOptions)
+            var linecharttooltip = $scope.linechart.addTooltip(linetooltipConfig);
+
+            function linetooltipConfig(d) {
+                linecharttooltip.html('GeneID：' + d.category + "<br>" + $scope.curType+"：" + d.key + "</br>" + "log10(FPKM+1): " + d.value+"<br>FPKM "+d.key+"："+d.value)
+            }
+
+            // 改色
+            (function changeColor() {
+                groupedbarGetItem();
+                var index = '';
+                function groupedbarGetItem() {
+                    $scope.linechart.getLegendItem(function (d, i) {
+                        reportService.selectColor(changeColorCallback);
+                        index = i;
+                    })
+                    function changeColorCallback(color2) {
+                        color = color2;
+                        $scope.linechart.changeColor(index, color2);
+                        $scope.lineOptions.dataBox.normalColor[index] = [color2];
+                        groupedbarChangeColor(color2);
+                    }
+                }
+                function groupedbarChangeColor(color) {
+                    $scope.linechart.redraw($('#analysis-linePanel .graph_header').eq(0).width() * 0.8);
+                    groupedbarGetItem();
+                }
+            })()
+        }
+
+
+        //get links 
+        $scope.linksError = false;
+        $scope.GetLinks = function () {
+            var promise = ajaxService.GetDeferData({
+                data: {},
+                url: options.api.java_url + "/analysis/parent/" + $scope.id
+            })
+            promise.then(function (res) {
+                if (res.status != 200) {
+                    $scope.linksError = "syserror";
+                } else {
+                    $scope.linksError = false;
+                    $scope.links = res.data.links;
+                }
+            }, function (err) {
+                console.log(err);
+            })
+        }
+
+        // 查看links
+        $scope.handlerSeeClick = function (item) {
+            var type = item.chartType || item.charType;
+            if (item.process == 0) {
+                $window.open('../tools/index.html#/home/error/' + item.id);
+            } else {
+                // success
+                $window.open('../tools/index.html#/home/' + type + '/' + item.id);
+            }
         }
     }
 });

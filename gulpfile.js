e0bf8835a83e290f0858ec14e323cae4ab923613
xiaofang <1692977129@qq.com>
2018-06-26 19:30:44 +0800

    gulp 打包mrna

--- gulpfile.js
+++ gulpfile.js
@@ -7,24 +7,18 @@ var gulp = require('gulp'),
 
 requireDir('./tasks', { recurse: true });
 
-gulp.task('build', ['include'], function () {
-    return gulp.start(['login', 'chipseq', 'rnaseq', 'miRNA', 'rna16s', 'gwas','dnaReseq','RADseq','mangerSystem']);
+gulp.task('build', ['include'], function() {
+    return gulp.start(['login', 'mrna', 'tools']);
 });
 
-gulp.task('default', function () {
+gulp.task('default', function() {
     log('用法： gulp <command> [--<arg> <value>]');
     log('下面是一些常用的任务命令：');
     log('   build   创建一个新的部署，默认部署到 production 文件夹中');
     log('   login   仅部署 login 模块，默认部署到 production 文件夹中');
-    log('   chipseq 仅部署 chipseq 模块，默认部署到 production 文件夹中');
-    log('   rnaseq  仅部署 rnaseq 模块，默认部署到 production 文件夹中');
-    log('   miRNA   仅部署 miRNA 模块，默认部署到 production 文件夹中');
-    log('   rna16s   仅部署 rna16s 模块，默认部署到 production 文件夹中');
-    log('   gwas   仅部署 gwas 模块，默认部署到 production 文件夹中');
-    log('   dnaReseq   仅部署 dnaReseq 模块，默认部署到 production 文件夹中');
-    log('   RADseq   仅部署 RADseq 模块，默认部署到 production 文件夹中');
-    log('   mangerSystem   仅部署 mangerSystem 模块，默认部署到 production 文件夹中');
     log('   include 仅部署 include 模块，默认部署到 production 文件夹中');
+    log('   mrna  仅部署 mrna 模块，默认部署到 production 文件夹中');
+    log('   tools  仅部署 tools 模块，默认部署到 production 文件夹中');
     log('常用的命令参数如下：');
     log('   --env 决定部署的文件夹，默认为 production ，除非部署到 production 文件夹，否则单独部署模块时，要提供此参数以指定目标文件夹！');
     log('注意：');
--- tasks/login.js
+++ tasks/login.js
@@ -1,17 +1,17 @@
-var gulp = require('gulp'),                                     // gulp
+var gulp = require('gulp'), // gulp
 
-    argv = require('yargs').argv,                               // 获取命令行参数
-    concat = require('gulp-concat'),                            // 文件连接
-    rename = require('gulp-rename'),                            // 文件重命名
-    notify = require('gulp-notify'),                            // gulp调用系统通知
-    del = require('del'),                                       // 清理
+    argv = require('yargs').argv, // 获取命令行参数
+    concat = require('gulp-concat'), // 文件连接
+    rename = require('gulp-rename'), // 文件重命名
+    notify = require('gulp-notify'), // gulp调用系统通知
+    del = require('del'), // 清理
 
-    amdOptimize = require("amd-optimize"),                      // AMD自动导入
-    minifyHTML = require('gulp-minify-html'),                   // 压缩HTML
-    uglify = require('gulp-uglify'),                            // 压缩js
+    amdOptimize = require("amd-optimize"), // AMD自动导入
+    minifyHTML = require('gulp-minify-html'), // 压缩HTML
+    uglify = require('gulp-uglify'), // 压缩js
 
-    rev = require('gulp-rev'),                                  // 生成MD5
-    revCollector = require('gulp-rev-collector'),               // 替换文件
+    rev = require('gulp-rev'), // 生成MD5
+    revCollector = require('gulp-rev-collector'), // 替换文件
     modules = require('../modules.json'),
     login = modules.login;
 
@@ -25,8 +25,8 @@ argv.build = env;
  * 依赖任务：login:html-rev，保证主页面中图片、CSS和JS路径被替换，且HTML被压缩
  * 注意：login模块与其他模块不同，所以写法略简单
  */
-gulp.task('login', ['login:html-rev'], function () {
-    return gulp.src('').pipe(notify('压缩login文件夹成功！')).on('end', function () {  
+gulp.task('login', ['login:html-rev'], function() {
+    return gulp.src('').pipe(notify('压缩login文件夹成功！')).on('end', function() {
         del([argv.build + login.js.dest + '/*.js', '!' + argv.build + login.js.dest + '/loginMain-*.js'])
     });
 });
@@ -36,8 +36,10 @@ gulp.task('login', ['login:html-rev'], function () {
  * 作用：替换login模块下页面中的图片、CSS和JS路径，且压缩HTML
  * 依赖任务：login:amd，保证AMD模块被导入
  */
-gulp.task('login:html-rev', ['login:amd'], function () {
-    return gulp.src([modules.rev + '/rev-common-images.json', modules.rev + '/rev-common-css.json', modules.rev + '/rev-login-main-js.json'].concat(login.html.main))
+gulp.task('login:html-rev', ['login:js-rev'], function() {
+    return gulp.src([modules.rev + '/rev-common-images.json', modules.rev + '/rev-common-css.json',
+            modules.rev + '/rev-login-css.json', modules.rev + '/rev-login-main-js.json', modules.rev + '/rev-login-images.json'
+        ].concat(login.html.main))
         .pipe(revCollector({ replaceReved: true }))
         .pipe(minifyHTML({
             empty: true,
@@ -47,11 +49,27 @@ gulp.task('login:html-rev', ['login:amd'], function () {
 })
 
 /**
+ * 任务：login:js-rev
+ * 作用：替换login模块中homeMain.js中引用的所有HTML和图片
+ * 依赖任务：login:amd，必须保证homeMain.js已执行AMD导入
+ */
+gulp.task('login:js-rev', ['login:amd'], function() {
+    return gulp.src([modules.rev + '/rev-login-html.json', login.js.main.replace('webroot', argv.build)])
+        .pipe(revCollector({ replaceReved: true }))
+        .pipe(uglify())
+        .pipe(rev())
+        .pipe(gulp.dest(argv.build + login.js.dest))
+        .pipe(rev.manifest({ merge: true }))
+        .pipe(rename('rev-login-main-js.json'))
+        .pipe(gulp.dest(modules.rev));
+})
+
+/**
  * 任务：login:amd
  * 作用：AMD导入，生成模块JS并压缩
  * 依赖任务：clean:login，删除login模块下的所有文件
  */
-gulp.task('login:amd', ['clean:login'], function () {
+gulp.task('login:amd', ['login:html'], function() {
     return gulp.src(login.js.src)
         .pipe(gulp.dest(argv.build + login.js.dest))
         .pipe(amdOptimize(login.js.module, {
@@ -59,20 +77,34 @@ gulp.task('login:amd', ['clean:login'], function () {
             baseUrl: argv.build + '/'
         }))
         .pipe(concat(login.js.file))
-        .pipe(uglify())
-        .pipe(rev())
-        .pipe(gulp.dest(argv.build + login.js.dest))
-        .pipe(rev.manifest({ merge: true }))
-        .pipe(rename('rev-login-main-js.json'))
-        .pipe(gulp.dest(modules.rev));
+        .pipe(gulp.dest(argv.build + login.js.dest));
 });
+
+/**
+ * 任务：login:html
+ * 作用：提供login模块非主页面的所有页面中图片和CSS路径，并对页面进行压缩
+ * 依赖任务：clean:login，清理文件夹
+ */
+gulp.task('login:html', ['clean:login'], function() {
+    return gulp.src([modules.rev + '/rev-login-images.json'].concat(login.html.src))
+        .pipe(revCollector({ replaceReved: true }))
+        .pipe(minifyHTML({
+            empty: true,
+            spare: true
+        }))
+        .pipe(rev())
+        .pipe(gulp.dest(argv.build + login.html.dest))
+        .pipe(rev.manifest({ merge: true }))
+        .pipe(rename('rev-login-html.json'))
+        .pipe(gulp.dest(modules.rev));
+})
 
 /**
  * 任务：clean:login
  * 作用：清理login模块文件夹
  */
-gulp.task('clean:login', function () {
-    return del(login.js.del.map(function (value) {
+gulp.task('clean:login', function() {
+    return del(login.js.del.map(function(value) {
         return argv.build + value;
     }));
 })

5d000a6060a73280e2e9f1b9f249cc651a996e01
yangwd <yangwd@gooalgene.com>
2018-07-06 15:05:35 +0800

    Add : help page

--- webroot/ps/tools/pages/helpController.js
+++ webroot/ps/tools/pages/helpController.js
@@ -1,15 +1,38 @@
-define(['toolsApp'], function(toolsApp) {
+define(['toolsApp'], function (toolsApp) {
     toolsApp.controller('helpController', helpController);
     helpController.$inject = ["$rootScope", "$scope", "$log", "$state", "$timeout", "$window", "$compile", "ajaxService", "toolService", "svgService", "reportService"];
 
     function helpController($rootScope, $scope, $log, $state, $timeout, $window, $compile, ajaxService, toolService, svgService, reportService) {
         toolService.pageLoading.open();
-        $scope.InitPage = function() {
-            $timeout(function() {
+        $scope.InitPage = function () {
+            $timeout(function () {
                 toolService.pageLoading.close();
             }, 300)
 
-            $scope.title= '帮助'
+            $scope.title = '帮助';
+        }
+
+        $scope.handlerAnchorClick = function (event) {
+            var target = event.target.getAttribute('data-target');
+            $('.help-anchor li').removeClass('active');
+            $(event.target).addClass('active');
+            var dis = $(target).offset().top;
+            $('#div_ViewProduct').off();
+            $('#div_ViewProduct').animate({ scrollTop: $('#div_ViewProduct').scrollTop() + dis }, function () {
+                $('#div_ViewProduct').on('scroll', scrollFn);
+            });
+        }
+
+        $('#div_ViewProduct').on('scroll', scrollFn);
+        function scrollFn() {
+            for (var i = 0; i < $('.help-title').length; i++) {
+                if ($('.help-title').eq(i).offset().top >= 0) {
+                    var target = $('.help-title').eq(i).attr('id');
+                    $('.help-anchor li').removeClass('active');
+                    $("[data-target=#" + target + "]").addClass('active');
+                    return;
+                }
+            }
         }
     }
 });

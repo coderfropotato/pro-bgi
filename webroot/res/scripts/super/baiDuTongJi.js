0a66a8880e587d0e435f72299b74467e46008f68
xiaofang <1692977129@qq.com>
2018-08-21 10:20:59 +0800

    add :bai du tong ji 测试版

---
+++ webroot/res/scripts/super/baiDuTongJi.js
@@ -0,0 +1,17 @@
+// 华大正式版
+// var _hmt = _hmt || [];
+// (function() {
+//     var hm = document.createElement("script");
+//     hm.src = "https://hm.baidu.com/hm.js?490df16b9f9f928f1b0213c793cff45d";
+//     var s = document.getElementsByTagName("script")[0];
+//     s.parentNode.insertBefore(hm, s);
+// })();
+
+// 华大测试版
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?47df917f516ab62a6ae2fe6b44684146";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();

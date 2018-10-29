9f9df58de711273a55e62723e1206d61e62609bc
yangwd <yangwd@gooalgene.com>
2018-10-29 11:25:57 +0800

    Update : input validate

--- webroot/res/scripts/super/directive/gridFilterDire.js
+++ webroot/res/scripts/super/directive/gridFilterDire.js
@@ -290,9 +290,9 @@ define("superApp.gridFilterDire", ["angular", "super.superMessage", "select2"],
                 }
             }, 30);
 
-            // 验证输入 非法字符\ / * ? : "<> | 
+            // 验证输入 排除 \ ? * < > “ ” @ # $ % ^ &
             $scope.keyup = function(searchText){
-                var reg = /[\\,\/,\?,\*,:,<,>,|,_]/g;
+                var reg = /[\\\?\*\<\>\"\"\@\#\$\%\^\&]/g;
                 if(reg.test($scope.filterFindEntity[searchText])){
                     $scope.filterFindEntity[searchText] = '';
                 }
